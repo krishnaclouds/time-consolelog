2972f54c449e3b8907bfa5a67a331d33b6f68938
krishnaclouds <prince.balakrishna@gmail.com>
2018-03-04 21:16:19 +0530

    Time Console Logging

---
+++ index.js
@@ -0,0 +1,19 @@
+const ts = require('time-stamp');
+
+/*!
+ * time-logger <https://github.com/krishnaclouds/time-consolelog>
+ */
+'use strict';
+
+
+/**
+ * Parse the given pattern and return a formatted
+ * timestamp.
+ *
+ * @param  {String} `msg` Message to Log.
+ * @return {String} `Console log message!`
+ */
+
+module.exports = function (msg) {
+    console.log("[" + ts() + "] " + msg);
+}
